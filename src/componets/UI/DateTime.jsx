dcb33576564c2cffd93af3ffbb10f239b25b2b1e
dylangarro28 <dylangarro28@gmail.com>
2024-08-17 23:29:59 +0000

    Cambios Realizados hasta  17/8/2024

--- src/componets/UI/DateTime.jsx
+++ src/componets/UI/DateTime.jsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { Container, Row, Col } from "reactstrap";
-
+//Hola
 
 export default function DateTime() {
     const [value, setValue] = React.useState(dayjs('2022-04-17T15:30'));
